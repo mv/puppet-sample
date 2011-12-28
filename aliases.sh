a1f4cb07324a127c6461cd607c16f9d6f5f2fb01
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2011-12-28 15:41:29 -0200

    Using for reference...

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+
+
+
+alias psg='ps -ef | grep -v grep | grep'
+alias netl='netstat -lanpt'
+alias ifc='/sbin/ifconfig'
+
+alias df='df -h'
+alias cp='cp -i'
+alias cp='cp -i'
+alias cp='cp -i'
+
+alias ls='ls -hAF --color'
+alias ll='ls -l'
+alias la='ls -a'
+
+set -o vi
+

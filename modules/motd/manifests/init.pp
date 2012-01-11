2fd805971bd62af42078ba59265809e861324239
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2012-01-11 15:34:19 -0200

    Motd simple: ok

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,12 +1,17 @@
 
 class motd {
 
-    file { "/etc/motd":
+    file { '/etc/motd':
         owner  => 'root',
         group  => 'root',
-        mode   => '644',
-        source => 'puppet:///modules/motd/motd.txt'
+        mode   => '664',
+        source => 'puppet:///modules/motd/motd.txt',
+        backup => '.bkp',
+        audit  => 'all',
     }
+
+
+
 
 }
 

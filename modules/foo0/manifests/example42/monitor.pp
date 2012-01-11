195b0f9948d425735cbdf919cc652b0692460c65
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2012-01-11 15:35:18 -0200

    Added.

---
+++ modules/foo0/manifests/example42/monitor.pp
@@ -0,0 +1,11 @@
+# Class foo::example42::monitor
+#
+# Custom monitor class for example42 project.
+# Here you can override or define your settings for the monitor wrapper
+#
+class foo::example42::monitor inherits foo::monitor {
+
+}
+
+# vim:ft=pp:
+

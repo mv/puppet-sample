12f2a35eac5cc86338df70c26d7bdcb42bd25dac
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2012-01-16 01:55:41 -0200

    devel3: using uninstall_force + run stages

---
+++ modules/devel3/manifests/install.pp
@@ -0,0 +1,21 @@
+# Class devel3::install
+#
+# It defines package, service, main configuration file.
+#
+# Usage:
+# include devel3
+#
+
+class devel3::install {
+
+    # Load variables defined in params.pp file.
+    require devel3::params
+
+    Package { ensure => present }
+
+    package { $devel3::params::rpm_list : }
+
+}
+
+# vim:ft=puppet:
+
---
+++ modules/devel3/manifests/remove.pp
@@ -0,0 +1,17 @@
+# Class devel3::remove
+#
+# Removes devel3 package and its relevant monitor, backup, firewall entries
+#
+# Usage:
+# include devel3::remove
+#
+
+class devel3::remove {
+
+    # wrapper
+    require devel3::absent
+
+}
+
+# vim:ft=puppet:
+
